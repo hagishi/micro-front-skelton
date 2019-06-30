3ddd7fdb1ef15b17458b3e56e81eea7a3b255894
hagishi <hagishi.c@gmail.com>
2019-06-30 20:54:22 +0900

    init

---
+++ app.ts
@@ -0,0 +1,6 @@
+import './app.scss';
+//
+const dom = document.getElementById('dom')!
+dom.addEventListener('click', () => {
+  console.log('click');
+})
---
+++ readme.md
@@ -0,0 +1,19 @@
+# micro front skelton
+
+## setup
+
+```
+yarn
+```
+
+## development
+
+```
+yarn dev
+```
+
+## build
+
+```
+yarn build
+```
